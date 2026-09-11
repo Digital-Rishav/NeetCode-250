4e2ee9c2cef0921b76ed1718604881b797a9f781
Rishav Kumar <164481815+Digital-Rishav@users.noreply.github.com>
2026-09-11 13:19:48 +0530

    Optimize array initialization for concatenation

--- Data Structures & Algorithms/concatenation-of-array/submission-0.java
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[nums.length * 2];
         int n = nums.length;
+        int[] ans = new int[2 * n];
+        
         for (int i=0; i<n; i++){
             ans[i] = nums[i];
             ans[i + n] = nums[i];
